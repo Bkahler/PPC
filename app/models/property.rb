5f553732e855fd9004f841b64ca3f46e30962b41
Bryan K <bkahler3@gmail.com>
2014-05-04 21:01:47 -0700

    Adds bootstap and limits search predicates

--- app/models/property.rb
+++ app/models/property.rb
@@ -3,5 +3,11 @@ class Property < ActiveRecord::Base
   has_many :owner_properties
   has_many :owners, through: :owner_properties
 
+UNRANSACKABLE_ATTRIBUTES = ["id", "published", "created_at"]
+
+def self.ransackable_attributes auth_object = nil
+  (column_names - UNRANSACKABLE_ATTRIBUTES) + _ransackers.keys
+end
+
 
 end
