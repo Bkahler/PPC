8587ff1d8f5c641e67186feda27a0fd51e8068d6
Bryan K <bkahler3@gmail.com>
2014-05-08 15:02:06 -0700

    allows for CSV Import for properties

--- app/models/property.rb
+++ app/models/property.rb
@@ -20,7 +20,9 @@ class Property < ActiveRecord::Base
 
   def self.import(file)
     CSV.foreach(file.path, headers: true) do |row|
-      Property.create! row.to_hash
+      params = {:apn=>row["apn"],:acres=>row["acres"],:GIS_acres=>row["GIS_acres"],:build_acres=>row["build_acres"],:year_sold=>row["year_sold"],:sale_price=>row["sale_price"],:assesment=>row["assesment"]}
+      prop = Property.create! params
+      prop.owners << Owner.find_by_id(row["owner_id"])
     end
   end
 
