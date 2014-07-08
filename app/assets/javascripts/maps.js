b9c5b15a518ce511abb643a446b54f50181cf92c
Bryan K <bkahler3@gmail.com>
2014-07-08 10:58:50 -0700

    moves JS out of view

---
+++ app/assets/javascripts/maps.js
@@ -0,0 +1,25 @@
+$(window).load(function(){
+  // for isotope call and to allow see all page to appear
+  // on pageload check to see if map div is on current page
+  // if so initialize map into #map div
+  if($('#map').length > 0){
+    initialize_map();
+  }
+});
+
+
+function initialize_map(){
+
+  var map = L.mapbox.map('map', 'examples.map-y7l23tes').setView([37.7, -122.4], 1);
+
+  $.get('/properties.json').done(function(data){
+
+    _.each(data,function(item){
+
+      if(item.geojson.properties.STATE_NAME == "Alaska"){
+        var thing = L.geoJson(item.geojson).addTo(map);
+      }
+
+    });
+  });
+}
